ceb316663e354b3c94c0a547b6eae992b846e700
folivetti <folivetti@ufabc.edu.br>
2023-08-03 09:26:17 -0300

    fisher of the distributions

--- src/Data/SRTree/Likelihoods.hs
+++ src/Data/SRTree/Likelihoods.hs
@@ -6,13 +6,15 @@ module Data.SRTree.Likelihoods
   , mse
   , rmse
   , nll
-  , gradNLL)
+  , gradNLL
+  , fisherNLL
+  )
     where
 
 import qualified Data.Vector as V
 import qualified Data.Vector.Storable as VS
 import qualified Numeric.LinearAlgebra as LA
-import Data.SRTree (SRTree (..), gradParamsRev, evalTree)
+import Data.SRTree (SRTree (..), gradParamsRev, evalTree, deriveBy, floatConstsToParam)
 import Data.SRTree.Recursion (Fix(..))
 import Data.Maybe ( fromMaybe )
 
@@ -35,6 +37,8 @@ mse xss ys tree theta = sse xss ys tree theta / fromIntegral (LA.size ys)
 rmse :: Columns -> Column -> Fix SRTree -> V.Vector Double -> Double
 rmse xss ys tree = sqrt . mse xss ys tree
 
+logit x = 1 / (1 + exp (-x))
+
 -- | Negative log-likelihood
 nll :: Distribution -> Maybe Double -> Columns -> Column -> Fix SRTree -> V.Vector Double -> Double
 nll Gaussian msErr xss ys t theta = 0.5*ssr/(sErr*sErr) + 0.5*m*log (2*pi*sErr*sErr)
@@ -43,11 +47,13 @@ nll Gaussian msErr xss ys t theta = 0.5*ssr/(sErr*sErr) + 0.5*m*log (2*pi*sErr*s
     ssr = sse xss ys t theta
     sErr = fromMaybe 1.0 msErr
 
+-- | Bernoulli distribution of f(x; theta) is, given phi = 1 / (1 + exp (-f(x; theta))),
+-- y log phi + (1-y) log (1 - phi), assuming y \in {0,1}
 nll Bernoulli _ xss ys tree theta
   | notValid ys = error "For Bernoulli distribution the output must be either 0 or 1."
   | otherwise   = negate . LA.sumElements $ VS.zipWith (\a b -> if a == 0.0 then log (1 - b) else log b) ys yhat
   where
-    yhat = evalTree xss theta LA.scalar tree
+    yhat = logit $ evalTree xss theta LA.scalar tree
     notValid = VS.any (\x -> x /= 0 && x /= 1)
 
 nll Poisson _ xss ys tree theta 
@@ -59,7 +65,7 @@ nll Poisson _ xss ys tree theta
 
 -- | Gradient of the negative log-likelihood
 gradNLL :: Distribution -> Maybe Double -> Columns -> Column -> Fix SRTree -> V.Vector Double -> LA.Vector Double
-gradNLL Gaussian msErr xss ys tree theta = LA.fromList [LA.sumElements (g * err) / sErr * sErr | g <-grad]
+gradNLL Gaussian msErr xss ys tree theta = LA.fromList [LA.sumElements (g * err) / sErr * sErr | g <- grad]
   where
     (yhat, grad) = gradParamsRev xss theta LA.scalar tree
     err          = yhat - ys
@@ -67,7 +73,7 @@ gradNLL Gaussian msErr xss ys tree theta = LA.fromList [LA.sumElements (g * err)
 
 gradNLL Bernoulli _ xss ys tree theta
   | notValid ys = error "For Bernoulli distribution the output must be either 0 or 1."
-  | otherwise   = LA.fromList [LA.sumElements $ (yhat - ys) * g / ((1 - yhat) * yhat) | g <- grad]
+  | otherwise   = LA.fromList [LA.sumElements $ (logit yhat - ys) * g | g <- grad]
   where
     (yhat, grad) = gradParamsRev xss theta LA.scalar tree
     notValid = VS.any (\x -> x /= 0 && x /= 1)
@@ -78,3 +84,35 @@ gradNLL Poisson _ xss ys tree theta
   where
     (yhat, grad) = gradParamsRev xss theta LA.scalar tree
     notValid = VS.any (<0)
+
+-- | Fisher information of negative log-likelihood
+fisherNLL :: Distribution -> Maybe Double -> Columns -> Column -> Fix SRTree -> V.Vector Double -> [Double]
+fisherNLL d msErr xss ys tree theta = do 
+  ix <- [0 .. p-1]
+  let f'      = deriveBy True ix t'
+      f''     = deriveBy True ix f'
+      fvals'  = evalTree xss theta LA.scalar f'
+      fvals'' = evalTree xss theta LA.scalar f''
+  pure $ diagHess d sErr ys yhat fvals' fvals''
+  where
+    p       = V.length theta
+    (t', _) = floatConstsToParam tree
+    yhat    = evalTree xss theta LA.scalar t'
+    ssr     = sse xss ys tree theta
+    sErr    = case msErr of
+                Nothing -> sqrt $ ssr / fromIntegral (LA.size ys - p)
+                Just x  -> x
+
+diagHess :: Distribution -> Double -> Column -> Column -> Column -> Column -> Double
+diagHess Gaussian sErr ys yhat fvals' fvals'' = sum f_ii / (sErr ^ 2)
+  where
+    f_ii = LA.toList $ fvals'^2 - res*fvals''
+    res  = ys - yhat
+
+diagHess Bernoulli _ ys yhat fvals' fvals'' = sum . LA.toList $ (1 - phi)*phi*fvals'^2 + (phi - ys)*fvals''
+  where phi = logit yhat
+
+diagHess Poisson _ ys yhat fvals' fvals'' = sum . LA.toList $ phi*fvals'^2 + (phi - ys)*fvals''
+  where phi = exp yhat
+
+-- | Hessian of negative log-likelihood
